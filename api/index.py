c17c6eac63b621a2ee387c60edbceb10f1ebe0ea
souhardyasengupta05 <24f3000363@ds.study.iitm.ac.in>
2025-10-05 15:54:31 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,8 +9,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["POST", "GET", "OPTIONS"],
+    allow_credentials=True,
+    allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"] # important
 )
 
 class LatencyRequest(BaseModel):
